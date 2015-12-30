bcdc6ffd9deac6e69f1ae4ce54f06fbd52cfb5ad
Jordan Sanderson <jordan.sanderson@cerner.com>
2015-12-30 15:14:30 -0600

    Edited the run script

--- src/main/resources/run.sh
+++ src/main/resources/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-java -Xms64m -Xmx256m -jar ./jars/${project.artifactId}-${project.version}.jar 2015-12-19 2015-12-20 http://webapps.rrc.state.tx.us/CMPL/
+java -Xms64m -Xmx256m -jar ./jars/${project.artifactId}-${project.version}.jar 2015-12-19 `date +%m-%d-%Y` http://webapps.rrc.state.tx.us/CMPL/
 mkdir -p archives
 mv ./wells.csv ./results-archives/wells-`date +%m-%d-%Y`.csv
 mv ./newWells.csv ./wells.csv
