1db000d8d0d5c64fcf2c603b17502b530f275f40
Jordan Sanderson <jordan.sanderson@cerner.com>
2015-12-30 15:12:24 -0600

    Edited logrotate script

--- src/main/resources/oilfield-logix-scraper-logrotate.sh
+++ src/main/resources/oilfield-logix-scraper-logrotate.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env
 # use to rotate logs on a monthly basis so the box doesn't get full
 mkdir -p /var/scraper-log-archives
+mkdir -p /var/scraper-results-archives
 tar cf ~/logs ~/scraper-logs-`date +%m-%Y`.tar
+tar cf ~/archives ~/scraper-results-`date +%m-%Y`.tar
 rm -rf ~/logs/*
-mv ~/scraper-logs-`date +%m-%Y`.tar /var/scraper-log-archives~/scraper-logs-`date +%m-%Y`.tar
+rm -rf ~/archives/*
+mv ~/scraper-logs-`date +%m-%Y`.tar /var/scraper-log-archives/scraper-logs-`date +%m-%Y`.tar
+mv ~/scraper-results-`date +%m-%Y`.tar /var/scraper-results-archives/scraper-logs-`date +%m-%Y`.tar
